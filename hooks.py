dcb4b1960322eef32b33dec79e046b046691b651
murthu <luckymurthu@gmail.com>
2025-12-09 17:30:48 +0530

    Delete hooks.py

--- hooks.py
+++
@@ -1,9 +0,0 @@
-app_name = "frappe_new"
-app_title = "Frappe New"
-app_publisher = "Murthu"
-app_description = "Test app for ERPNext Marketplace"
-app_email = "luckymurthu@gmail.com"
-app_license = "MIT"
-
-# These are required for Frappe validation
-app_version = "0.0.1"
